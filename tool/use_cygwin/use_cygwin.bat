19272ce657ab584334b52c46f3bfbec9d231a078
Gennaro Prota <gennaro.prota+github@gmail.com>
2020-08-25 09:44:31 +0200

    Change "REM" to "rem" for batch file comments
    Reason: aesthetics.
    
    Other keywords: case.

--- tool/use_cygwin/use_cygwin.bat
+++ tool/use_cygwin/use_cygwin.bat
@@ -1,20 +1,20 @@
 @echo off
-REM ==========================================================================
-REM                        Copyright 2013 Gennaro Prota
-REM
-REM                  Licensed under the 3-Clause BSD License.
-REM             (See accompanying file 3_CLAUSE_BSD_LICENSE.txt or
-REM              <https://opensource.org/licenses/BSD-3-Clause>.)
-REM __________________________________________________________________________
-REM
-REM     A quick tool to invoke before compiling with Cygwin, on Windows.
-REM
-REM     The only important thing that this does is putting /usr/bin at
-REM     the *end* of %PATH%, so that Cygwin's `link' isn't found before
-REM     the homonym Microsoft linker.
-REM
-REM     Usage: see the top-level read_me.
-REM --------------------------------------------------------------------------
+rem ==========================================================================
+rem                        Copyright 2013 Gennaro Prota
+rem
+rem                  Licensed under the 3-Clause BSD License.
+rem             (See accompanying file 3_CLAUSE_BSD_LICENSE.txt or
+rem              <https://opensource.org/licenses/BSD-3-Clause>.)
+rem __________________________________________________________________________
+rem
+rem     A quick tool to invoke before compiling with Cygwin, on Windows.
+rem
+rem     The only important thing that this does is putting /usr/bin at
+rem     the *end* of %PATH%, so that Cygwin's `link' isn't found before
+rem     the homonym Microsoft linker.
+rem
+rem     Usage: see the top-level read_me.
+rem --------------------------------------------------------------------------
 
 setlocal
 
@@ -24,9 +24,9 @@ set CYGWIN_ROOT=C:\cygwin64
 
 endlocal
 
-REM Local Variables:
-REM mode: bat
-REM indent-tabs-mode: nil
-REM batch-indent-level: 4
-REM End:
-REM vim: set ft=dosbatch et sts=4 sw=4:
+rem Local Variables:
+rem mode: bat
+rem indent-tabs-mode: nil
+rem batch-indent-level: 4
+rem End:
+rem vim: set ft=dosbatch et sts=4 sw=4:
