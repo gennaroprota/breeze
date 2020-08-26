a81e757088b268cc6cb0818b1ea6b9bbf354a737
Gennaro Prota <gennaro.prota+github@gmail.com>
2020-08-26 13:10:40 +0200

    Hyphenate "namespace scope" in the documentation
    Other keywords: spelling, hyphenation.

--- breath/operator/equality_comparison.hpp
+++ breath/operator/equality_comparison.hpp
@@ -23,9 +23,9 @@ namespace adl_shield {
 //      ====================
 //
 //!     The class template \c equality_comparison provides <code>
-//!     operator ==()</code> and <code>operator !=()</code> as namespace
-//!     scope functions whose name is associated "on demand" (i.e. at
-//!     instantiation) to class \c T, via ADL.
+//!     operator ==()</code> and <code>operator !=()</code> as
+//!     namespace-scope functions whose name is associated "on demand"
+//!     (i.e. at instantiation) to class \c T, via ADL.
 //!
 //!     Usage:
 //!     ------
