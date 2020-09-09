4155e90b4be6fd7f69c044eaa8a0fc68fead8fdc
Gennaro Prota <gennaro.prota+github@gmail.com>
2020-09-09 16:56:59 +0200

    Make some get_cpuid_info() documentation effectively appear
    AFAIK, it couldn't appear if written as before, because it wasn't
    associated to any object by Doxygen.

--- breath/cpu/get_cpuid_info.hpp
+++ breath/cpu/get_cpuid_info.hpp
@@ -7,31 +7,8 @@
 // ___________________________________________________________________________
 //
 //!     \file
-//!     \brief Interface to the CPUID instruction.
-// ---------------------------------------------------------------------------
-//
-//!     Interface to the CPUID instruction available on x86 and x86_64
-//!     architectures.
-//!
-//!     NOTE: unusually for our code base, this file uses extern "C";
-//!     that's because the actual routines are written in assembly and
-//!     it is just easier to write portable assembly that way (no name
-//!     mangling differences, for example). The fact that the routines
-//!     are written in assembly however is an implementation detail
-//!     (which I'm mentioning just to provide the rationale for extern
-//!     "C").
-//!
-//!     Once you use extern "C", then, you're better off not using a
-//!     namespace (because it would be ignored by the linker anyway).
-//!     So, the names here are *global*.
-//!
-//!     Finally, note that all the values (either in input and output)
-//!     are 32-bit. This is not by mistake: the CPUID instruction still
-//!     uses 32-bit values even in 64-bit mode.
-//!
-//!     It's intended that these functions are the lowest level before
-//!     dealing with assembly. And that higher level interfaces be
-//!     defined upper in the library/application logic.
+//!     \brief Interface to the CPUID instruction available on x86 and
+//!            x86_64 architectures.
 // ---------------------------------------------------------------------------
 
 #include <cstdint>
@@ -59,7 +36,7 @@ struct cpuid_result
 //!         Executes the CPUID instruction with the provided values in
 //!         \c EAX and \c ECX.
 //!
-//!     Namespace: global (see above).
+//!     Namespace: global (see below).
 //!
 //!     \pre
 //!         The CPUID instruction is supported (this is always true on
@@ -68,6 +45,28 @@ struct cpuid_result
 //!     \return
 //!         A struct with the values of \c EAX, \c EBX, \c ECX, \c EDX
 //!         returned by the CPUID instruction itself.
+//!
+//!     \note
+//!         Unusually for our code base, this file uses extern "C";
+//!         that's because the actual routines are written in assembly
+//!         and it is just easier to write portable assembly that way
+//!         (no name mangling differences, for example). The fact that
+//!         the routines are written in assembly however is an
+//!         implementation detail (which I'm mentioning just to provide
+//!         the rationale for extern "C").
+//!     \note
+//!         Once you use extern "C", then, you're better off not using a
+//!         namespace (because it would be ignored by the linker
+//!         anyway). So, the names here are *global*.
+//!     \note
+//!         Finally, note that all the values (either in input and
+//!         output) are 32-bit. This is not by mistake: the CPUID
+//!         instruction still uses 32-bit values even in 64-bit mode.
+//!     \note
+//!         It's intended that these functions are the lowest level
+//!         before dealing with assembly. And that higher level
+//!         interfaces be defined upper in the library/application
+//!         logic.
 // ---------------------------------------------------------------------------
 cpuid_result        get_cpuid_info( std::uint32_t eax, std::uint32_t ecx ) ;
 
