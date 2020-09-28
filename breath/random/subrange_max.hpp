66f801c0822088d14a4cf526f6ff4d0fd7395ed5
Gennaro Prota <gennaro.prota+github@gmail.com>
2020-09-28 09:58:25 +0200

    Improve the formatting of the subrange_max() documentation
    Other keywords: TrueType, <tt>, fixed width, spacing, bullet lists.

--- breath/random/subrange_max.hpp
+++ breath/random/subrange_max.hpp
@@ -22,33 +22,37 @@ namespace breath_ns {
 //
 //!     \return
 //!         The maximum value that can be kept (before taking the
-//!         remainder of the division by x+1) for getting a random (or
-//!         pseudorandom) integer in the range [0, x] from a source that
-//!         spans [0, m], x <= m.
+//!         remainder of the division by <tt>x + 1</tt>) for getting a
+//!         random (or pseudorandom) integer in the range <tt>[0, x]
+//!         </tt> from a source that spans <tt>[0, m]</tt>, <tt>x <= m
+//!         </tt>.
 //!
-//!     Typical example: wanting a value in [0, 5] (dice) from [0,
-//!     RAND_MAX]; if e.g. RAND_MAX == 32767 then subrange_max(5,
-//!     RAND_MAX) returns
+//!     Typical example: wanting a value in <tt>[0, 5]</tt> (dice) from
+//!     <tt>[0, RAND_MAX]</tt>; if e.g. <tt>RAND_MAX == 32767</tt> then
+//!     <tt>subrange_max(5, RAND_MAX)</tt> returns
 //!
-//!         32765
+//!         <tt>32765</tt>
 //!
 //!     This allows discarding only a tiny fraction of the values in
 //!     the whole range of the source (only the last two, in fact),
 //!     rather than discarding anything greater than or equal to six.
 //!
-//!     Why that value? The idea, of course, is that there are 32766
-//!     numbers in [0, 32765], and that is a multiple of six (the
-//!     number of values in [0, 5]). So, the remainders 0, 1, 2, 3, 4
-//!     and 5 all appear with the same probability (if the source is
-//!     not biased). If we took the whole range then, of course, 0 and
-//!     1 would be more frequent than the others.
+//!     Why that value? The idea, of course, is that there are <tt>32766
+//!     </tt>numbers in <tt>[0, 32765]</tt>, and that is a multiple of
+//!     six (the number of values in <tt>[0, 5]</tt>). So, the
+//!     remainders <tt>0</tt>, <tt>1</tt>, <tt>2</tt>, <tt>3</tt>, <tt>4
+//!     </tt> and <tt>5</tt> all appear with the same probability (if
+//!     the source is not biased). If we took the whole range then, of
+//!     course, <tt>0</tt> and <tt>1</tt> would be more frequent than
+//!     the others.
 //!
-//!     With respect to just discarding anything greater than x:
+//!     With respect to just discarding anything greater than <tt>x
+//!     </tt>:
 //!
-//!         For pseudorandom numbers this basically saves execution
-//!         time.
-//!         For true random numbers it avoids needlessly discarding
-//!         numbers and thus consuming precious entropy.
+//!      - For pseudorandom numbers this basically saves execution time.
+//!
+//!      - For true random numbers it avoids needlessly discarding
+//!        numbers and thus consuming precious entropy.
 //!
 //!     \pre
 //!         0 <= x <= m   (i.e. destination range not wider
