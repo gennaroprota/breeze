c35d20fe1e815b547a5137b4d7630c03e754545b
Gennaro Prota <gennaro.prota+github@gmail.com>
2020-10-16 12:55:56 +0200

    Complete the documentation of windows_version_info::service_pack_level()
    This documents the type of exceptions emitted. Note that, since we might
    be able to implement it with different APIs that don't set
    GetLastError(), I'm not guaranteeing that the type of the exceptions is
    last_api_error.

--- breath/environment/windows_only/windows_version_info.hpp
+++ breath/environment/windows_only/windows_version_info.hpp
@@ -79,6 +79,10 @@ public:
     //!     \return
     //!         The service pack level. Zero if no service pack is
     //!         installed.
+    //!
+    //!     \throw
+    //!         A \c std::runtime_error (which can be a \c
+    //!         last_api_error) if an error occurs.
     // -----------------------------------------------------------------------
     static int          service_pack_level() ;
 
