f551026f67566770fcf0eeaf00e3fa08eb68440d
Gennaro Prota <gennaro.prota+github@gmail.com>
2020-12-05 03:09:43 +0100

    Document the checks done by the counter template
    Reason: they are part of the contract; that's one of the points of the
    template (the other one being prohibiting operations that make no sense
    for a counter). In this respect, documenting n >=0 as a *precondition*
    for one of the constructors was incorrect.
    
    Other keywords: documentation, guarantees.

--- breeze/counting/counter.hpp
+++ breeze/counting/counter.hpp
@@ -8,7 +8,8 @@
 //
 //!     \file
 //!     \brief A counter providing only operations meaningful for a
-//!            counter, plus overflow and underflow checking.
+//!            counter, plus checking of the initial value, of overflow
+//!            and underflow.
 // ---------------------------------------------------------------------------
 
 #ifndef BREEZE_GUARD_igDqQSfWTOavMKrliVx0QKSbOMZ1k4kD
@@ -23,6 +24,12 @@ namespace breeze_ns {
 //      ========
 //
 //!     \copybrief counter.hpp
+//!
+//!     This template is a bit unusual, in that it performs checks via
+//!     \c BREEZE_ASSERT() which are part of its contract (see e.g. the
+//!     documentation of the \c ++ and \c \-- operator functions),
+//!     whereas \c BREEZE_ASSERT() is normally used to check for \e
+//!     precondition violations, only.
 // ---------------------------------------------------------------------------
 template< typename IntegralType >
 class counter
@@ -36,26 +43,57 @@ public:
 
     //!     Constructs a counter with internal value \c n.
     //!
-    //!     \pre
-    //!         n >= 0
+    //!     This constructor executes:
+    //!
+    //!     \code
+    //!         BREEZE_ASSERT( n >= 0 ) ;
+    //!     \endcode
     // -----------------------------------------------------------------------
     explicit            counter( IntegralType n ) ;
 
     //!     Pre-increments the counter. Can only be called on lvalues.
+    //!
+    //!
+    //!     This functions checks for logical overflow (including if \c
+    //!     IntegralType is unsigned), by executing:
+    //!
+    //!     \code
+    //!         BREEZE_ASSERT(
+    //!             value() < std::numeric_limits< IntegralType >::max() ) ;
+    //!     \endcode
+    //!
+    //!     before incrementing the counter value.
     // -----------------------------------------------------------------------
     counter &           operator ++() & ;
 
     //!     Post-increments the counter. Can only be called on
     //!     lvalues.
+    //!
+    //!     Before incrementing the counter value, this function does
+    //!     the same \c BREEZE_ASSERT() check as the pre-increment
+    //!     function.
     // -----------------------------------------------------------------------
     counter             operator ++( int ) & ;
 
     //!     Pre-decrements the counter. Can only be called on lvalues.
+    //!
+    //!     This function checks for logical underflow (including if \c
+    //!     IntegralType is unsigned), by executing:
+    //!
+    //!     \code
+    //!         BREEZE_ASSERT( value() > 0 ) ;
+    //!     \endcode
+    //!
+    //!     before decrementing the counter value.
     // -----------------------------------------------------------------------
     counter &           operator --() & ;
 
     //!     Post-decrements the counter. Can only be called on
     //!     lvalues.
+    //!
+    //!     Before decrementing the counter value, this function does
+    //!     the same \c BREEZE_ASSERT() check as the pre-decrement
+    //!     function.
     // -----------------------------------------------------------------------
     counter             operator --( int ) & ;
 
