5f9589bff403ee321ad05ecdb1a2de8ae5b55e48
Gennaro Prota <gennaro.prota+github@gmail.com>
2021-01-27 12:20:45 +0100

    Define some internal macros in the usual order (arch/system/compiler)
    Reason: convenience, immediateness of reading.

--- breeze/porting/dependent_source.hpp
+++ breeze/porting/dependent_source.hpp
@@ -151,13 +151,13 @@
     BREEZE_PASTE( BREEZE_DEPENDENT_SOURCE_, variant )( file_name )         /**/
 
 
-#define BREEZE_DEPENDENT_SOURCE_compiler( file_name )                         \
-    BREEZE_STRINGIZE_AFTER_EXPANSION( dep/comp/BREEZE_COMPILER/file_name ) /**/
-
 #define BREEZE_DEPENDENT_SOURCE_system_family( file_name )                         \
     BREEZE_STRINGIZE_AFTER_EXPANSION( dep/syst/BREEZE_SYSTEM_FAMILY/file_name ) /**/
 
 #define BREEZE_DEPENDENT_SOURCE_system( file_name )                           \
     BREEZE_STRINGIZE_AFTER_EXPANSION( dep/syst/BREEZE_SYSTEM/file_name )   /**/
+
+#define BREEZE_DEPENDENT_SOURCE_compiler( file_name )                         \
+    BREEZE_STRINGIZE_AFTER_EXPANSION( dep/comp/BREEZE_COMPILER/file_name ) /**/
 
 #endif
