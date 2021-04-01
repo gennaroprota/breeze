a240d1a1e8897ff73200970827bc8efb30548991
Gennaro Prota <gennaro.prota+github@gmail.com>
2021-04-01 15:03:56 +0200

    Make the to_string() tests less verbose

--- breeze/text/test/to_string_test.cpp
+++ breeze/text/test/to_string_test.cpp
@@ -21,32 +21,34 @@ namespace {
 
 void do_tests()
 {
-    BREEZE_CHECK( breeze::to_string( 12 ) == "12" ) ;
-    BREEZE_CHECK( breeze::to_string( " hello, world " ) == " hello, world " ) ;
-    BREEZE_CHECK( breeze::to_string( "" ) == "" ) ;
-    BREEZE_CHECK( breeze::to_string( 'a' ) == "a" ) ;
+    using breeze::to_string ;
+    using namespace std::chrono;
+
+    BREEZE_CHECK( to_string( 12 ) == "12" ) ;
+    BREEZE_CHECK( to_string( " hello, world " ) == " hello, world " ) ;
+    BREEZE_CHECK( to_string( "" ) == "" ) ;
+    BREEZE_CHECK( to_string( 'a' ) == "a" ) ;
 
     //      Conversion from double. Note that these are not the same
     //      results as std::to_string().
     // -----------------------------------------------------------------------
-    BREEZE_CHECK( breeze::to_string( 12.34 ) == "12.34" ) ;
-    BREEZE_CHECK( breeze::to_string( 1e-10 ) == "1e-10" ) ;
-    BREEZE_CHECK( breeze::to_string( 1e+50 ) == "1e+50" ) ;
-    BREEZE_CHECK( breeze::to_string( 1e-50 ) == "1e-50" ) ;
-    BREEZE_CHECK( breeze::to_string( 123456789.0 ) == "1.23457e+08" ) ;
+    BREEZE_CHECK( to_string( 12.34 ) == "12.34" ) ;
+    BREEZE_CHECK( to_string( 1e-10 ) == "1e-10" ) ;
+    BREEZE_CHECK( to_string( 1e+50 ) == "1e+50" ) ;
+    BREEZE_CHECK( to_string( 1e-50 ) == "1e-50" ) ;
+    BREEZE_CHECK( to_string( 123456789.0 ) == "1.23457e+08" ) ;
 
     //      Conversions of std::chrono::durations.
     // -----------------------------------------------------------------------
-    BREEZE_CHECK( breeze::to_string( std::chrono::duration< double,
+    BREEZE_CHECK( to_string( duration< double,
         std::pico >( 100.1 ) ) == "100.1ps" ) ;
-    BREEZE_CHECK( breeze::to_string(
-        std::chrono::milliseconds( 2 ) ) == "2ms" ) ;
-    BREEZE_CHECK( breeze::to_string( std::chrono::seconds( 5 ) ) == "5s" ) ;
-    BREEZE_CHECK( breeze::to_string( std::chrono::duration< int,
+    BREEZE_CHECK( to_string( milliseconds( 2 ) ) == "2ms" ) ;
+    BREEZE_CHECK( to_string( seconds(      5 ) ) == "5s"  ) ;
+    BREEZE_CHECK( to_string( duration< int,
                 std::ratio< 120, 2 > >( 25 ) ) == "25min" ) ;
-    BREEZE_CHECK( breeze::to_string( std::chrono::duration< int,
+    BREEZE_CHECK( to_string( duration< int,
                 std::ratio< 3, 4 > >( 2 ) ) == "2[3/4]s" ) ;
-    BREEZE_CHECK( breeze::to_string( std::chrono::hours( 50 ) ) == "50h" ) ;
+    BREEZE_CHECK( to_string( hours( 50 ) ) == "50h" ) ;
 }
 
 }
