de5b9b2e6742de5db8902432116177a66adbfe75
Gennaro Prota <gennaro.prota+github@gmail.com>
2021-04-23 12:47:11 +0200

    Add a missing const qualifier
    Note: for once, I also made a formatting change, without separating the
    commit.
    
    Other keywords: variable name indentation, formatting style.

--- breeze/uuid/test/uuid_test.cpp
+++ breeze/uuid/test/uuid_test.cpp
@@ -85,8 +85,8 @@ uuids_are_distinct()
 
     //      Outputting directly from here is all but elegant... (TODO?)
     // -----------------------------------------------------------------------
-    std::vector< breeze::uuid >::const_iterator it =
-        std::adjacent_find( uuids.begin(), uuids.end() ) ;
+    std::vector< breeze::uuid >::const_iterator const
+                        it = std::adjacent_find( uuids.begin(), uuids.end() ) ;
     if ( it != uuids.end() ) {
         std::cerr << "    Duplicated uuid's: " << std::endl ;
         std::cerr << "    " << *it             << std::endl ;
