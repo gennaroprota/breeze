8111b51fee2d575b9867e0987ff7f0c006645434
Gennaro Prota <gennaro.prota+github@gmail.com>
2021-05-10 10:45:35 +0200

    Settle the documentation of max_for_subrange()

--- breeze/random/max_for_subrange.hpp
+++ breeze/random/max_for_subrange.hpp
@@ -29,32 +29,31 @@ namespace breeze_ns {
 //!         </tt> from a source that spans <tt>[0, m]</tt>, <tt>x <= m
 //!         </tt>.
 //!
-//!     Typical example: wanting a value in <tt>[0, 5]</tt> (dice) from
-//!     <tt>[0, RAND_MAX]</tt>; if e.g. <tt>RAND_MAX == 32767</tt> then
-//!     <tt>max_for_subrange(5, RAND_MAX)</tt> returns
-//!
-//!         <tt>32765</tt>
+//!     Typical example: wanting a value in <tt>[0, 5]</tt> (die roll)
+//!     from <tt>[0, RAND_MAX]</tt>; if e.g. <tt>RAND_MAX == 32767</tt>
+//!     then <tt>max_for_subrange(5, RAND_MAX)</tt> returns
+//!     <tt>32765</tt>.
 //!
 //!     This allows discarding only a tiny fraction of the values in the
 //!     whole range of the source (only the last two, in fact), rather
-//!     than discarding anything greater than or equal to six.
+//!     than discarding anything greater than five.
 //!
-//!     Why that value? The idea, of course, is that there are <tt>32766
-//!     </tt>numbers in <tt>[0, 32765]</tt>, and that is a multiple of
-//!     six (the number of values in <tt>[0, 5]</tt>). So, the
-//!     remainders <tt>0</tt>, <tt>1</tt>, <tt>2</tt>, <tt>3</tt>, <tt>4
-//!     </tt> and <tt>5</tt> all appear with the same probability (if
-//!     the source is not biased). If we took the whole range then, of
-//!     course, <tt>0</tt> and <tt>1</tt> would be more frequent than
+//!     Why that value? The idea, of course, is that there are
+//!     <tt>32766</tt> integers in <tt>[0, 32765]</tt>, and that is a
+//!     multiple of six (the number of integers in <tt>[0, 5]</tt>). So,
+//!     the remainders <tt>0</tt>, <tt>1</tt>, <tt>2</tt>, <tt>3</tt>,
+//!     <tt>4</tt> and <tt>5</tt> all appear with the same probability
+//!     (if the source is not biased). If we took the whole range then,
+//!     of course, <tt>0</tt> and <tt>1</tt> would be more frequent than
 //!     the others.
 //!
-//!     With respect to just discarding anything greater than <tt>x
-//!     </tt>:
+//!     With respect to just discarding anything greater than
+//!     <tt>x</tt>:
 //!
-//!      - For pseudo-random numbers this basically saves execution
+//!      - For pseudo-random numbers, this basically saves execution
 //!        time.
 //!
-//!      - For true random numbers it avoids needlessly discarding
+//!      - For true random numbers, it also avoids needlessly discarding
 //!        numbers and thus consuming precious entropy.
 //!
 //!     \pre
