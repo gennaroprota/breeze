b8563048547e2b49a651039ce44bfac70e302f7a
Gennaro Prota <gennaro.prota+github@gmail.com>
2021-06-09 11:07:38 +0200

    Extend check_include_guards.py with an additional check
    The additional check concerns including top_level_namespace.hpp if
    required.

--- tool/check_include_files/check_include_files.py
+++ tool/check_include_files/check_include_files.py
@@ -8,11 +8,15 @@
 # ____________________________________________________________________________
 
 """
-        Checks our include guards.
+        Checks our include files.
 
-        This script checks for mishaps in our include guards.
-        Specifically, that every .hpp file has one guard (and only one),
-        and that the names of the guarding macros are all distinct.
+        This script checks for mishaps in our include files.
+        Specifically, that every .hpp file other than
+        breeze/top_level_namespace.hpp that opens namespace breeze_ns
+        includes breeze/top_level_namespace.hpp in its first include
+        directive, that every .hpp file has one include guard (and only
+        one), and that the names of the guarding macros are all
+        distinct.
 
         It is tailored to our coding style, and is particularly simple,
         given that we don't use conditional compilation for anything
@@ -27,15 +31,17 @@ import sys
 
 argc = len( sys.argv )
 if ( argc != 2 ):
-    sys.exit( "Usage: check_include_guards.py <root_dir_path>" )
+    sys.exit( "Usage: check_include_files.py <root_dir_path>" )
 
 root = sys.argv[ 1 ]
 if ( not os.path.isdir( root ) ):
     sys.exit( "The specified directory was not found" )
 
-regex        = re.compile( "^#ifndef (BREEZE_GUARD_\w+)" )
-macro_dict   = collections.defaultdict( list )
-guard_counts = collections.defaultdict( int )
+regex                 = re.compile( "^#ifndef (BREEZE_GUARD_\w+)" )
+macro_dict            = collections.defaultdict( list )
+guard_counts          = collections.defaultdict( int )
+opens_namespace       = collections.defaultdict( bool )
+has_namespace_include = collections.defaultdict( bool )
 
 exit_code  = 0
 exit_error = 2
@@ -45,7 +51,19 @@ try:
             if name.endswith( ".hpp" ):
                 full_name = os.path.join( dir_path, name )
                 guard_counts[ full_name ] = 0
+                first_include_seen = False
+                opens_namespace[ full_name ] = False
+                has_namespace_include[ full_name ] = False
                 for line in open( full_name ):
+                    if line.find( "namespace breeze_ns" ) == 0:
+                        opens_namespace[ full_name ] = True
+
+                    if not first_include_seen and line.find( "#include" ) == 0:
+                        first_include_seen = True
+
+                        if line.find( '#include "breeze/top_level_namespace.hpp"') == 0:
+                            has_namespace_include[ full_name ] = True
+
                     m = regex.match( line )
                     if m:
                         guard_counts[ full_name ] += 1
@@ -55,6 +73,13 @@ except Exception as ex:
     print( "An exception occurred: {}".format( ex ), file = sys.stderr )
     exit_code = exit_error
 
+for file_name in opens_namespace:
+    if os.path.basename( file_name) != "top_level_namespace.hpp"    \
+            and opens_namespace[ file_name ]                        \
+            and not has_namespace_include[ file_name ]:
+        print( "Error: {} doesn't include top_level_namespace.hpp,"
+               " or includes it after other files".format( file_name ),
+               file = sys.stderr )
 
 for file_name in guard_counts:
     if guard_counts[ file_name ] != 1:
